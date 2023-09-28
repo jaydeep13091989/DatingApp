5eb4cf00c1db1e1342cb24856a0a612ae63690f4
jaydeep viradiya <jaydeep.viradiya@infogen-labs.com>
2023-09-28 18:19:03 +0530

    Migrating Stuff

--- API/Program.cs
+++ API/Program.cs
@@ -1,49 +1,86 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Hosting;
+using API.Extensions;
+using API.Middleware;
+using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace API
+var builder = WebApplication.CreateBuilder(args);
+
+// services Container
+var connString = "";
+if (builder.Environment.IsDevelopment())
+  connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-          
-          var host = CreateHostBuilder(args).Build();
-          using var scope = host.Services.CreateScope();
-          var services = scope.ServiceProvider;
-          try
-          {
-            var context = services.GetRequiredService<DataContext>();
-            var userManager = services.GetRequiredService<UserManager<AppUser>>();
-             var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-            await context.Database.MigrateAsync();
-            await Seed.ClearConnection(context);
-            //context.Connections.RemoveRange(context.Connections);
-            await Seed.SeedUsers(userManager, roleManager);
-          }
-          catch(Exception ex)
-          {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred during migration");
-          }
-          await host.RunAsync();
-        }
+  // Use connection string provided at runtime by Heroku.
+  var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+  // Parse connection URL to connection string for Npgsql
+  connUrl = connUrl.Replace("postgres://", string.Empty);
+  var pgUserPass = connUrl.Split("@")[0];
+  var pgHostPortDb = connUrl.Split("@")[1];
+  var pgHostPort = pgHostPortDb.Split("/")[0];
+  var pgDb = pgHostPortDb.Split("/")[1];
+  var pgUser = pgUserPass.Split(":")[0];
+  var pgPass = pgUserPass.Split(":")[1];
+  var pgHost = pgHostPort.Split(":")[0];
+  var pgPort = pgHostPort.Split(":")[1];
+  var updatedHost = pgHost.Replace("flycast", "internal");
+
+  connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+  opt.UseNpgsql(connString);
+});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddIdentityservices(builder.Configuration);
+
+
+//Middleware
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200"));
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+  var context = services.GetRequiredService<DataContext>();
+  var userManager = services.GetRequiredService<UserManager<AppUser>>();
+  var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+  await context.Database.MigrateAsync();
+  await Seed.ClearConnection(context);
+  //context.Connections.RemoveRange(context.Connections);
+  await Seed.SeedUsers(userManager, roleManager);
+}
+catch (Exception ex)
+{
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "An error occurred during migration");
+}
+app.Run();
+
